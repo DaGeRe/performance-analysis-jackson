b1756137214edc4340703e41045170aa5a75bb19
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-03-31 17:07:19 +0200

    Only get graph if file exists

--- getPerformanceGraphs.sh
+++ getPerformanceGraphs.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/plokhotnyuk/jsoniter-scala.git
+#git clone https://github.com/plokhotnyuk/jsoniter-scala.git
 cd jsoniter-scala
 git checkout gh-pages
 for benchmark in $(cat corretto-11.json | grep benchmark | tr -d "\"," | awk '{print $3}')
@@ -7,7 +7,14 @@ do
 	benchmarkFile=../$benchmark.csv
 	for commit in $(git log --pretty=format:"%h")
 	do 
-		git checkout $commit; echo -n "$commit " >> $benchmarkFile
+		git checkout $commit
+		if [ ! -f corretto-11.json ]
+		then
+			git reset --hard
+			git checkout gh-pages
+			break
+		fi
+		echo -n "$commit " >> $benchmarkFile
        		grep "ADTReading.avSystemGenCodec" corretto-11.json -A 40 | \
 			grep "score\"\|scoreError" | \
 			awk '{print $3}' | \
